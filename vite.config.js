882801c1686b2ac34be3c9cc145436d9f32e6e3b
LanishaThomas <lanishathomas7606@email.com>
2026-02-15 12:43:39 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  base: "/expense-tracker/"
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    base: command === "build"
+      ? "/expense-tracker/"
+      : "/"
+  }
 })
